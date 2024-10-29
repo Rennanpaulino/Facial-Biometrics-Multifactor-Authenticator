206952c7e66c14d44fd4ae3700cfff12af4d0f27
Rennanpaulino <rennanpsousa2003@gmail.com>
2024-10-29 20:45:12 -0300

    first commit

---
+++ cadastro.py
@@ -0,0 +1,9 @@
+class Pessoa:
+    def __init__(self, nome, cpf, email, access_level):
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.access_level = access_level
+        pass
+
+
